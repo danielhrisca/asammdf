c7cb9bf3419ad93cd8ec40f89342333bb0386553
danielhrisca <daniel.hrisca@gmail.com>
2017-11-27 21:34:38 +0200

    add recorder/streaming functionality

---
+++ asammdf/recorder.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+""" MDF version 4 recorder """
+
+
+
+class Recorder(object):
+    """ record MDF version 4 file.
+
+    Parameters
+    ----------
+    filename : str
+        output file name
+
+    """
+
+    def __init__(self, filename):
+
