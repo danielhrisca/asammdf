2c7172e914ae2610c9385c95816cd1c6dd33b735
danielhrisca <daniel.hrisca@gmail.com>
2017-12-13 10:56:48 +0200

    limit CI jobs to stable Python versions enable tests for MDf version 2

---
+++ test/test_mdf2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+import sys
+import unittest
+
+from utils import get_test_data
+from asammdf import MDF2
+
+
+
+class TestMDF3(unittest.TestCase):
+
+    def test_measurement(self):
+        self.assertTrue(MDF2)
+
+    def test_read_mdf2_00(self):
+        self.assertTrue(MDF2(get_test_data('test_meas_2.00.mdf')))
+
+
+if __name__ == '__main__':
+    unittest.main()
