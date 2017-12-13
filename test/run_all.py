b0ff63e3465e1faa7daee83dc423a57dc208900c
yahym <umihai1@yahoo.com>
2017-12-13 23:47:07 +0200

    - move pylint to after_success to have the build failed if tests are failed - it seems that travis does not accumulate the returned values

--- test/run_all.py
+++ test/run_all.py
@@ -16,7 +16,7 @@ def main():
     tests = unittest.TestLoader().discover('.', 'test_*.py')
     testResult = xmlrunner.XMLTestRunner(output='test-reports').run(tests)
 
-    return int(not testResult.wasSuccessful())
+    return not testResult.wasSuccessful()
 
 if __name__ == '__main__':
     sys.exit(main())
