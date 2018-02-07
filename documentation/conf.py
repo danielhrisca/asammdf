8e8235b6f50217b9bacbc74cd0f4463fe95da777
danielhrisca <daniel.hrisca@gmail.com>
2018-02-07 10:19:18 +0200

    switch to sphinx rtd theme

--- documentation/conf.py
+++ documentation/conf.py
@@ -29,28 +29,9 @@ with open(os.path.join('..', 'asammdf', 'version.py'), 'r') as f:
             break
 
 print('version', asam_version)
-import guzzle_sphinx_theme
 
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
-# Guzzle theme options (see theme.conf for more information)
-# Register the theme as an extension to generate a sitemap.xml
+html_theme = "sphinx_rtd_theme"
 
-
-# Guzzle theme options (see theme.conf for more information)
-html_theme_options = {
-
-    # Set the name of the project to appear in the left sidebar.
-    "project_nav_name": "asammdf {}".format(asam_version),
-
-
-
-    # Allow a separate homepage from the master_doc
-    "homepage": "index",
-
-    # Allow the project link to be overriden to a custom URL.
-    "projectlink": "http://github.com/danielhrisca/asammdf",
-}
 
 # -- General configuration ------------------------------------------------
 
