0b7119f3c33ae6edbd9a9a8af49c426438c57152
danielhrisca <daniel.hrisca@gmail.com>
2018-02-09 21:54:53 +0200

    add logo to documentation

--- documentation/conf.py
+++ documentation/conf.py
@@ -119,6 +119,7 @@ todo_include_todos = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+html_logo = '../asammdf.png'
 
 
 # -- Options for HTMLHelp output ------------------------------------------
