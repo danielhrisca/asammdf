d0410f1c47667381d5d89cbcdbe9f0bf32bc14f2
danielhrisca <daniel.hrisca@gmail.com>
2018-05-24 17:05:48 +0300

    * tabular values dialog

--- asammdfgui/gui.py
+++ asammdfgui/gui.py
@@ -1295,20 +1295,26 @@ class TabularValuesDialog(QDialog):
 
         self.table = QTableWidget(self)
 
-        header = []
+        self.header = []
         for sig in signals:
-            header.append('t [s]')
-            header.append('{} ({})'.format(sig.name, sig.unit))
+            self.header.append('t [s]')
+            self.header.append('{} ({})'.format(sig.name, sig.unit))
 
         self.table.setColumnCount(2*len(signals))
         self.table.setRowCount(
             max(len(sig) for sig in signals)
         )
-        self.table.setHorizontalHeaderLabels(header)
+        self.table.setHorizontalHeaderLabels(self.header)
 
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.table.horizontalHeader().setMinimumSectionSize(QHeaderView.Stretch)
-        self.table.horizontalHeader().setToolTip('cacasc')
+        self.table.horizontalHeader().setToolTip('')
+        self.table.horizontalHeader().setMinimumSectionSize(100)
+        self.table.horizontalHeader().sectionClicked.connect(self.show_name)
+        self.table.horizontalHeader().entered.connect(self.hover)
+        self.table.cellEntered.connect(self.hover)
+
+        print(dir(self.table.horizontalHeader()))
 
         for i, sig in enumerate(signals):
             range_ = ranges[i]
@@ -1372,6 +1378,14 @@ class TabularValuesDialog(QDialog):
             (screen.width() - 1200) // 2,
             (screen.height() - 600) // 2,
         )
+
+    def hover(self, row, column):
+        print('hover', row, column)
+
+    def show_name(self, index):
+        name = self.header[index//2]
+        widget = self.table.horizontalHeader()
+        QToolTip.showText(widget.mapToGlobal(QPoint(0, 0)), name)
 
 
 class ChannelStats(QWidget):
