252f7065a15a488482760bb5ee748a81ec7305d7
danielhrisca <daniel.hrisca@gmail.com>
2018-09-11 12:31:09 +0300

    * switch from rst to md in setup.py * remove extra folders from tar.gz

--- setup.py
+++ setup.py
@@ -12,15 +12,13 @@ from os import path, listdir, walk
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-long_description = open('README.rst', 'rb').read().decode('utf-8')
+long_description = open('README.md', 'rb').read().decode('utf-8')
 
 with open(path.join('asammdf', 'version.py'), 'r') as f:
     for line in f:
         if line.startswith('__version__'):
             version = line.split('=')[-1].strip().strip("'")
             break
-
-print(repr(long_description))
 
 setup(
     name='asammdf',
