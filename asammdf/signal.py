4924278997b7a6dce13553cf4d11ab246c516c69
danielhrisca <daniel.hrisca@gmail.com>
2018-12-01 12:37:09 +0200

    fix Signal extend method

--- asammdf/signal.py
+++ asammdf/signal.py
@@ -664,14 +664,12 @@ class Signal(object):
         """
         if len(self.timestamps):
             last_stamp = self.timestamps[-1]
-            delta = last_stamp / len(self) + last_stamp
         else:
             last_stamp = 0
-            delta = 0
         if len(other):
             other_first_sample = other.timestamps[0]
             if last_stamp >= other_first_sample:
-                timestamps = other.timestamps + delta - other_first_sample
+                timestamps = other.timestamps + last_stamp
             else:
                 timestamps = other.timestamps
 
