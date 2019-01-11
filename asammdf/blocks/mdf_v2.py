2c6265fce513b18671c112f28f560adfdce776ae
danielhrisca <daniel.hrisca@gmail.com>
2019-01-11 22:51:23 +0200

    remove memory argument for MDF v2

--- asammdf/blocks/mdf_v2.py
+++ asammdf/blocks/mdf_v2.py
@@ -4,7 +4,7 @@
 from __future__ import division, print_function
 
 from .mdf_v3 import MDF3
-from .utils import validate_memory_argument, validate_version_argument
+from .utils import validate_version_argument
 
 
 __all__ = ["MDF2"]
@@ -17,11 +17,10 @@ class MDF2(MDF3):
 
     """ shared implementation for mdf version 2 and 3 """
 
-    def __init__(self, name=None, memory="full", version="2.14", **kwargs):
-        memory = validate_memory_argument(memory)
+    def __init__(self, name=None, version="2.14", **kwargs):
         version = validate_version_argument(version, hint=2)
 
-        super(MDF2, self).__init__(name, memory, version, **kwargs)
+        super(MDF2, self).__init__(name, version, **kwargs)
 
 
 if __name__ == "__main__":
