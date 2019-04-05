5dda3e7daff6db67c3d5d4c5da7516334b80d5fa
danielhrisca <daniel.hrisca@gmail.com>
2019-04-05 22:54:32 +0300

    fine tune numeric window

--- asammdf/gui/widgets/numeric.py
+++ asammdf/gui/widgets/numeric.py
@@ -20,6 +20,7 @@ class Numeric(QtWidgets.QWidget):
         super().__init__()
         self.signals = []
         self._min = self._max = 0
+        self.format = 'phys'
         uic.loadUi(HERE.joinpath("..", "ui", "numeric.ui"), self)
 
         self.timestamp.valueChanged.connect(self._timestamp_changed)
@@ -36,36 +37,28 @@ class Numeric(QtWidgets.QWidget):
         self._min = self._max = 0
         items = []
         for i, sig in enumerate(signals):
-            if sig.samples.dtype.kind == "f":
-                sig.format = "phys"
-                sig.plot_texts = None
-            else:
-                sig.format = "phys"
-                if sig.samples.dtype.kind in "SV":
-                    sig.plot_texts = sig.texts = sig.samples
-                    sig.samples = np.zeros(len(sig.samples))
+            sig.kind = sig.samples.dtype.kind
+            size = len(sig)
+            sig.size = size
+            if size:
+                if sig.kind == 'f':
+                    value = f'{sig.samples[0]:.6f}'
                 else:
-                    sig.plot_texts = None
-            color = COLORS[i % 10]
-            sig.color = color
-            if len(sig):
+                    value = str(sig.samples[0])
                 self._min = min(self._min, sig.timestamps[0])
                 self._max = max(self._max, sig.timestamps[-1])
-                sig.empty = False
-                value = f'{sig.samples[0]:.6f}'
             else:
-                sig.empty = True
                 value = 'n.a.'
 
             items.append(
-                QtWidgets.QTreeWidgetItem([sig.name, sig.unit, value])
+                QtWidgets.QTreeWidgetItem([sig.name, value, sig.unit])
             )
         self.channels.addTopLevelItems(items)
 
         self.timestamp.setRange(self._min, self._max)
         self.min_t.setText(f'{self._min:.3f}s')
         self.max_t.setText(f'{self._max:.3f}s')
-        self._update_values(self.timestamp.value())
+        self._update_values()
 
     def _timestamp_changed(self, stamp):
         val = int((stamp - self._min) / (self._max - self._min) * 9999)
@@ -82,22 +75,84 @@ class Numeric(QtWidgets.QWidget):
 
         self._update_values(val)
 
-    def _update_values(self, stamp):
+    def _update_values(self, stamp=None):
+        if stamp is None:
+            stamp = self.timestamp.value()
         iterator = QtWidgets.QTreeWidgetItemIterator(self.channels)
 
-        index = 0
-        while 1:
-            item = iterator.value()
-            if not item:
-                break
-            sig = self.signals[index]
-            if not sig.empty:
-                idx = searchsorted(sig.timestamps, stamp)
-                item.setText(2, f'{sig.samples[idx]:.6f}')
+        if self.format == 'bin':
 
-            index += 1
-            iterator += 1
+            index = 0
+            while 1:
+                item = iterator.value()
+                if not item:
+                    break
+                sig = self.signals[index]
+                if sig.size:
+                    value = sig.samples[min(sig.size-1, searchsorted(sig.timestamps, stamp))]
+
+                    if sig.kind == 'f':
+                        item.setText(1, f'{value:.6f}')
+                    elif sig.kind in 'ui':
+                        item.setText(1, bin(value))
+                    else:
+                        item.setText(1, str(value))
+
+                index += 1
+                iterator += 1
+        elif self.format == 'hex':
+
+            index = 0
+            while 1:
+                item = iterator.value()
+                if not item:
+                    break
+                sig = self.signals[index]
+                if sig.size:
+                    value = sig.samples[min(sig.size-1, searchsorted(sig.timestamps, stamp))]
+
+                    if sig.kind == 'f':
+                        item.setText(1, f'{value:.6f}')
+                    elif sig.kind in 'ui':
+                        item.setText(1, f'0x{value:X}')
+                    else:
+                        item.setText(1, str(value))
+
+                index += 1
+                iterator += 1
+        else:
+            index = 0
+            while 1:
+                item = iterator.value()
+                if not item:
+                    break
+                sig = self.signals[index]
+                if sig.size:
+                    value = sig.samples[min(sig.size-1, searchsorted(sig.timestamps, stamp))]
+                    if sig.kind == 'f':
+                        item.setText(1, f'{value:.6f}')
+                    else:
+                        item.setText(1, str(value))
+
+                index += 1
+                iterator += 1
 
     def add_new_channel(self, sig):
         if sig:
             self.add_signals(self.signals + [sig,])
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        modifier = event.modifiers()
+
+        if key in (QtCore.Qt.Key_H, QtCore.Qt.Key_B, QtCore.Qt.Key_P) and modifier == QtCore.Qt.ControlModifier:
+            if key == QtCore.Qt.Key_H:
+                self.format = 'hex'
+            elif key == QtCore.Qt.Key_B:
+                self.format = 'bin'
+            else:
+                self.format = 'phys'
+            self._update_values()
+            event.accept()
+        else:
+            super().keyPressEvent(event)
