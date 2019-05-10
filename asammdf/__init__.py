2b7e6f76afb6c160ad9f137e655c657062152546
danielhrisca <daniel.hrisca@gmail.com>
2019-05-10 10:47:56 +0300

    expose plot function

--- asammdf/__init__.py
+++ asammdf/__init__.py
@@ -16,10 +16,11 @@ from .blocks.mdf_v2 import MDF2
 from .blocks.mdf_v3 import MDF3
 from .blocks.mdf_v4 import MDF4
 from .mdf import MDF, SUPPORTED_VERSIONS
+from .plot import plot
 from .signal import Signal
 from .version import __version__
 
 
 __all__ = [
-    "__version__", "MDF", "MDF2", "MDF3", "MDF4", "Signal", "SUPPORTED_VERSIONS",
+    "__version__", "MDF", "MDF2", "MDF3", "MDF4", "Signal", "SUPPORTED_VERSIONS", "plot"
 ]
