98f138db3f552404aa6eeef0cec6a88d214af559
danielhrisca <daniel.hrisca@gmail.com>
2019-05-10 10:49:30 +0300

    fix wrong import

--- asammdf/__init__.py
+++ asammdf/__init__.py
@@ -16,7 +16,7 @@ from .blocks.mdf_v2 import MDF2
 from .blocks.mdf_v3 import MDF3
 from .blocks.mdf_v4 import MDF4
 from .mdf import MDF, SUPPORTED_VERSIONS
-from .plot import plot
+from .gui import plot
 from .signal import Signal
 from .version import __version__
 
