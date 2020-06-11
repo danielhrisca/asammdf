cbb0e129cf87974e7b5b740ce5f04167a3d1c045
danielhrisca <daniel.hrisca@gmail.com>
2020-06-11 08:46:01 +0300

    fix typo

--- asammdf/blocks/mdf_v4.py
+++ asammdf/blocks/mdf_v4.py
@@ -6877,7 +6877,7 @@ class MDF4(object):
                         vals_dtype = vals.dtype.kind
                         if kind_ == "b":
                             pass
-                        elif len(shape_) > 1 and not in (v4c.DATA_TYPE_BYTEARRAY, v4c.DATA_TYPE_MIME_SAMPLE, v4c.DATA_TYPE_MIME_STREAM):
+                        elif len(shape_) > 1 and data_type not in (v4c.DATA_TYPE_BYTEARRAY, v4c.DATA_TYPE_MIME_SAMPLE, v4c.DATA_TYPE_MIME_STREAM):
                             vals = self._get_not_byte_aligned_data(
                                 data_bytes, grp, ch_nr
                             )
