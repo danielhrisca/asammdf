84c143ff66c14d761de49bdd75bb71adac1cc29d
danielhrisca <daniel.hrisca@gmail.com>
2020-11-04 08:58:48 +0200

    add class attributes for common Source

--- asammdf/blocks/source_utils.py
+++ asammdf/blocks/source_utils.py
@@ -6,6 +6,7 @@ asammdf utility functions for source information
 from . import v2_v3_blocks as v3b
 from . import v2_v3_constants as v3c
 from . import v4_blocks as v4b
+from . import v4_constants as v4c
 
 __all__ = ["Source"]
 
@@ -13,6 +14,23 @@ __all__ = ["Source"]
 class Source:
 
     __slots__ = "name", "path", "comment", "source_type", "bus_type"
+
+    SOURCE_OTHER = v4c.SOURCE_OTHER
+    SOURCE_ECU = v4c.SOURCE_ECU
+    SOURCE_BUS = v4c.SOURCE_BUS
+    SOURCE_IO = v4c.SOURCE_IO
+    SOURCE_TOOL = v4c.SOURCE_TOOL
+    SOURCE_USER = v4c.SOURCE_USER
+
+    BUS_TYPE_NONE = v4c.BUS_TYPE_NONE
+    BUS_TYPE_OTHER = v4c.BUS_TYPE_OTHER
+    BUS_TYPE_CAN = v4c.BUS_TYPE_CAN
+    BUS_TYPE_LIN = v4c.BUS_TYPE_LIN
+    BUS_TYPE_MOST = v4c.BUS_TYPE_MOST
+    BUS_TYPE_FLEXRAY = v4c.BUS_TYPE_FLEXRAY
+    BUS_TYPE_K_LINE = v4c.BUS_TYPE_K_LINE
+    BUS_TYPE_ETHERNET = v4c.BUS_TYPE_ETHERNET
+    BUS_TYPE_USB = v4c.BUS_TYPE_USB
 
     def __init__(self, name, path, comment, source_type, bus_type):
         """Commons reprezentation for source information
@@ -47,16 +65,16 @@ class Source:
                     source.name,
                     source.path,
                     source.comment,
-                    0,  # source type other
-                    0,  # bus type none
+                    self.SOURCE_OTHER,  # source type other
+                    self.BUS_TYPE_NONE,  # bus type none
                 )
             else:
                 source = cls(
                     source.name,
                     source.path,
                     source.comment,
-                    2,  # source type bus
-                    2,  # bus type CAN
+                    self.SOURCE_BUS,  # source type bus
+                    self.BUS_TYPE_CAN,  # bus type CAN
                 )
 
         elif isinstance(source, v4b.SourceInformation):
