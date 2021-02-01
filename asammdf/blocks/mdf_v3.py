aa42f57fb7aae537e3d3c1fe4624a6346c9134f9
danielhrisca <daniel.hrisca@gmail.com>
2021-02-01 17:17:04 +0200

    another missing attribute

--- asammdf/blocks/mdf_v3.py
+++ asammdf/blocks/mdf_v3.py
@@ -173,6 +173,7 @@ class MDF3(MDF_Common):
         self._float_interpolation = 1
         self._raise_on_multiple_occurrences = True
         self._use_display_names = False
+        self.copy_on_get = False
 
         self._si_map = {}
         self._cc_map = {}
