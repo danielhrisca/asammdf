93d7fe660357b414da5cc52e5617acb1fd572b23
JulienGrv <grave.jul@gmail.com>
2021-02-26 22:44:52 +0100

    make export dependencies optional

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        "canmatrix[arxml, dbc]>=0.8",
+        "canmatrix[arxml,dbc]>=0.8",
         "lz4",
         "numexpr",
         "numpy>=1.16.1",
@@ -98,18 +98,7 @@ setup(
     # $ pip install -e .[dev,test]
     extras_require={
         "decode": ["cChardet==2.1.5", "chardet"],
-        "gui": [
-            "lxml",
-            "natsort",
-            "psutil",
-            "PyQt5>=5.13.1",
-            "pyqtgraph==0.11.0",
-            "h5py",
-            "fastparquet",
-            "scipy",
-            "hdf5storage",
-            "snappy",
-        ],
+        "gui": ["lxml", "natsort", "psutil", "PyQt5>=5.13.1", "pyqtgraph==0.11.0"],
         "encryption": "cryptography",
     },
     # If there are data files included in your packages that need to be
@@ -125,6 +114,6 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={"console_scripts": ["asammdf=asammdf.gui.asammdfgui:main"]},
+    entry_points={"console_scripts": ["asammdf = asammdf.gui.asammdfgui:main [gui]"]},
     ext_modules=_get_ext_modules(),
 )
