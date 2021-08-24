1f21cbeeb736a180e0f37e5e428ea2edffb335be
danielhrisca <daniel.hrisca@gmail.com>
2021-08-24 16:34:45 +0300

    speed-ups

--- asammdf/gui/asammdfgui.py
+++ asammdf/gui/asammdfgui.py
@@ -2,15 +2,34 @@
 import argparse
 import sys
 
+
 from PyQt5 import QtWidgets
 import pyqtgraph
 
+def _keys(self, styles):
+    def getId(obj):
+        try:
+            return obj._id
+        except AttributeError:
+            obj._id = next(pyqtgraph.graphicsItems.ScatterPlotItem.SymbolAtlas._idGenerator)
+            return obj._id
+
+    res = [
+        (symbol if isinstance(symbol, (str, int)) else getId(symbol), size, getId(pen), getId(brush))
+        for symbol, size, pen, brush in styles[:1]
+    ]
+    
+    return res
+
+# speed-up monkey patches
 pyqtgraph.graphicsItems.ScatterPlotItem._USE_QRECT = False
+pyqtgraph.graphicsItems.ScatterPlotItem.SymbolAtlas._keys = _keys
 
 from asammdf.gui.utils import excepthook
 from asammdf.gui.widgets.main import MainWindow
 
 sys.excepthook = excepthook
+
 
 
 def _cmd_line_parser():
