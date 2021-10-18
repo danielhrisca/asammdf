04a1bdcc3f812480363ac024e58381e38b2d7173
JulienGrv <grave.jul@gmail.com>
2021-10-18 17:47:03 +0200

    feat(blocks): add type annotations in mdf_common and source_utils

--- asammdf/types.py
+++ asammdf/types.py
@@ -10,12 +10,12 @@ from numpy.typing import NDArray
 from typing_extensions import Literal
 
 from .blocks import v2_v3_blocks, v4_blocks
-from .blocks.source_utils import Source
 
 if TYPE_CHECKING:
     from .blocks.mdf_v2 import MDF2
     from .blocks.mdf_v3 import MDF3
     from .blocks.mdf_v4 import MDF4
+    from .blocks.source_utils import Source
 
 StrPath = Union[str, "PathLike[str]"]
 StrOrBytesPath = Union[str, bytes, "PathLike[str]", "PathLike[bytes]"]
@@ -36,5 +36,5 @@ InputType = Union[BytesIO, StrPath, ZipFile, BZ2File, GzipFile]
 IntInterpolationModeType = Literal[0, 1, 2]
 MDF_v2_v3_v4 = Union["MDF2", "MDF3", "MDF4"]
 RasterType = Union[float, str, NDArray[Any]]
-SourceType = Union[v2_v3_blocks.ChannelExtension, v4_blocks.SourceInformation, Source]
+SourceType = Union[v2_v3_blocks.ChannelExtension, v4_blocks.SourceInformation, "Source"]
 SyncType = Literal[0, 1, 2, 3, 4]
