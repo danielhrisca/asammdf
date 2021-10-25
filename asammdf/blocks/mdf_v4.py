7ee983db23f2f71d0fbdf289a7f21a7f6dd00f4c
danielhrisca <daniel.hrisca@gmail.com>
2021-10-25 07:32:52 +0300

    fix

--- asammdf/blocks/mdf_v4.py
+++ asammdf/blocks/mdf_v4.py
@@ -7167,6 +7167,9 @@ class MDF4(MDF_Common):
             record_size += channel_group.invalidation_bytes_nr
 
             count = 0
+
+            if one_piece:
+                data = (data,)
             
             for fragment in data:
                 data_bytes, offset, _count, invalidation_bytes = fragment
