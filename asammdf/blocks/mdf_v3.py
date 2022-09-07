92ff430cfa00ff150b5c18a5ee3fb2ef9b0b4cc5
danielhrisca <daniel.hrisca@gmail.com>
2022-09-07 09:39:33 +0300

    debug 2

--- asammdf/blocks/mdf_v3.py
+++ asammdf/blocks/mdf_v3.py
@@ -3104,9 +3104,10 @@ class MDF3(MDF_Common):
 
         if vals.dtype.kind == "S":
             encoding = "latin-1"
-            print('>>>>', vals)
+            v = vals
             vals = array([e.rsplit(b"\0")[0] for e in vals.tolist()])
-            print('<<<<',vals)
+            if vals.dtype.kind != 'S':
+                print('>>>>', v, v.dtype, vals, vals.dtype)
 
         if samples_only:
             res = vals, None
