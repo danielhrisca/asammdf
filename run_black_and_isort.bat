b925305ea6433e09d45cfc0ae7550870be04c47f
danielhrisca <daniel.hrisca@gmail.com>
2023-04-04 09:34:30 +0300

    run on the setup as well

--- run_black_and_isort.bat
+++ run_black_and_isort.bat
@@ -1,5 +1,7 @@
 pip install -U black isort && ^
 black --config pyproject.toml . && ^
 black --config pyproject.toml asammdf.spec && ^
+black --config pyproject.toml setup.py && ^
 isort --settings-path pyproject.toml asammdf.spec && ^
+isort --settings-path pyproject.toml setup.py && ^
 isort --settings-path pyproject.toml .
