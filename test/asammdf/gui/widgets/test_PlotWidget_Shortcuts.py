a076243ce038029970c696a07893f63902d99de9
FBK <fillbk99@gmail.com>
2023-10-30 15:09:15 +0200

    todo

--- test/asammdf/gui/widgets/test_PlotWidget_Shortcuts.py
+++ test/asammdf/gui/widgets/test_PlotWidget_Shortcuts.py
@@ -5,11 +5,9 @@ from unittest import mock
 
 from PySide6 import QtCore, QtGui, QtTest
 
-import asammdf.gui.dialogs.messagebox
-import src.asammdf.gui.widgets.cursor
-
 
 class TestShortcutsWOChannels(TestPlotWidget):
+
     def __init__(self, methodName: str = ...):
         super().__init__(methodName)
 
@@ -428,6 +426,7 @@ class TestShortcutsWOChannels(TestPlotWidget):
 
 
 class TestShortcutsWith_1_Channel(TestPlotWidget):
+
     def __init__(self, methodName: str = ...):
         super().__init__(methodName)
 
@@ -862,6 +861,7 @@ class TestShortcutsWith_1_Channel(TestPlotWidget):
 
 
 class TestShortcutsWith_2_Channels(TestPlotWidget):
+
     def __init__(self, methodName: str = ...):
         super().__init__(methodName)
 
@@ -1055,8 +1055,17 @@ class TestShortcutsWith_2_Channels(TestPlotWidget):
         self.assertLess(old_from_to_x_channel_37[0], new_from_to_x_channel_37[0])
         self.assertLess(old_from_to_x_channel_37[1], new_from_to_x_channel_37[1])
 
+    def test_Plot_Plot_Shortcut_Key_Shift_Arrow(self):
+        """
+
+        """
+        # with mock.patch("") as mo_:
+        QtTest.QTest.keySequence(self.plot.channel_selection, QtGui.QKeySequence("Ctrl+Shift+S"))
+        self.manual_use(self.widget)
+
 
 class TestShortcutsWith_3_Channels(TestPlotWidget):
+
     def __init__(self, methodName: str = ...):
         super().__init__(methodName)
 
