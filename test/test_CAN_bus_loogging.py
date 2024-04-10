d9f05bac03d58bd1dccdae0b3211fae103cdbec4
Klint Anders (Consultant) <anders.klint@consultant.volvo.com>
2024-04-10 15:07:14 +0200

    corrected getting dir

--- test/test_CAN_bus_loogging.py
+++ test/test_CAN_bus_loogging.py
@@ -92,16 +92,16 @@ class TestCANBusLogging(unittest.TestCase):
         # This dbc throws exception without the suggested changes in branch "relaxed_j1939"...
         # else it is identical to the CSS Electronics demo file in test package
         # NB: This fiddöing with optional "test" in path not needed if test file included in test .zip!
-        d = os.abspath(__file__)
+        d = os.path.dirname(__file__)
         print(f"dir for running testfile: {d}")
-        dbc = os.path.join(d, "test", "almost-J1939.dbc")  # Local tests
+        dbc = os.path.join(d, "almost-J1939.dbc")  # Local tests
         print(f"{dbc=} exists? {os.path.exists(dbc)}")
         print(f"working dir for tests: {os.path.abspath('.')}")
         print(f"content of working dir: {os.listdir('.')}")
         print(f"content of working dir (abspath): {os.listdir(os.path.abspath('.'))}")
         print(f"content of ./test subdir: {os.listdir(os.path.join('.','test'))}")
         assert(False)  # We want fail and output of prints above
-        
+
         # if not os.path.exists(dbc):
         #    dbc = os.path.join(os.path.abspath("."), "almost-J1939.dbc")  # CI env?
 
