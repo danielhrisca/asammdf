1f8a51c6aed07e620da0e89992c7145fa86e5b16
danielhrisca <daniel.hrisca@gmail.com>
2024-04-17 08:51:30 +0300

    try to build with python 3.12

--- setup.py
+++ setup.py
@@ -74,7 +74,6 @@ setup(
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
