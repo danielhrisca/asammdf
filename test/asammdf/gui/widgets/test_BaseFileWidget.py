f0b69cc47c26feb52bef6ea4464c8d1d2ca092ef
Bodnariuc Florin (uif30975) <florin.bodnariuc@continental-corporation.com>
2024-06-27 15:41:28 +0300

    retry ,,,

--- test/asammdf/gui/widgets/test_BaseFileWidget.py
+++ test/asammdf/gui/widgets/test_BaseFileWidget.py
@@ -27,7 +27,7 @@ class TestFileWidget(TestBase):
         if hasattr(self._outcome.result, "failures") and hasattr(self._outcome.result, "errors"):
             if (self._outcome.result.failures or self._outcome.result.errors) and self.save_ss_here is not None:
                 self.widget.grab().save(os.path.join(
-                    self.screenshots, f"{sys.platform}_{platform.python_version().split(".")[1]}_{self.id()}.png")
+                    self.screenshots, f"{sys.platform}_{platform.python_version()}_{self.id()}.png")
                 )
         if self.widget:
             self.widget.close()
