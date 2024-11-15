12ad524fda630b3768795fd8ad89cd3e9d75682b
danielhrisca <daniel.hrisca@gmail.com>
2024-11-15 16:20:44 +0200

    independent folder

--- setup.py
+++ setup.py
@@ -135,6 +135,6 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={"console_scripts": ["asammdf = asammdf.asammdfgui:main [gui,export,decode]"]},
+    entry_points={"console_scripts": ["asammdf = asammdf.app.asammdfgui:main [gui,export,decode]"]},
     ext_modules=_get_ext_modules(),
 )
