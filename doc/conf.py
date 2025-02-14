2ef8cc10a327bb6c6b2e96538e11e1c6727040ea
danielhrisca <daniel.hrisca@gmail.com>
2025-02-14 08:27:28 +0200

    copyright years

--- doc/conf.py
+++ doc/conf.py
@@ -70,7 +70,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "asammdf"
-copyright = "2024, Daniel Hrisca"
+copyright = "2018-2025, Daniel Hrisca"
 author = "Daniel Hrisca"
 
 # The version info for the project you're documenting, acts as replacement for
